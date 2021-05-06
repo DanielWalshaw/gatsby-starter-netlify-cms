42ad121fb6ecc6b9073f9dff15b84ba99e1673b3
DanielWalshaw <82993177+DanielWalshaw@users.noreply.github.com>
2021-05-06 12:09:16 +1000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,16 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    // Tell gatsby which theme you will be using and other configs.
+		{
+		resolve: "gatsby-woocommerce-theme",
+		options: {
+			wordPressUrl: 'https://new.nextrend.com.au',
+			gatsbySiteUrl: 'https://quench.media',
+			googleTagManagerId: 'xxxx',
+			fbAppId: 'xxxx'
+		}
+	} 
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
